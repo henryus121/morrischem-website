46d11ed95bfc8191665a5f9efbae3030a869308b
henryus121 <henry@morrischem.com>
2026-08-02 14:43:41 +0300

    Add index.php for main site layout

---
+++ morrischem-industrial-clean/index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main class="site-main">
+    <div class="content-container">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post();
+                ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+                <?php
+            endwhile;
+        else :
+            ?>
+            <p>No content found.</p>
+            <?php
+        endif;
+        ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
