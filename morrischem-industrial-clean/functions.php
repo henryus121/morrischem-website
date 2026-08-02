82dd3ab9fa28ee315df53b873a9d907a88914bce
henryus121 <henry@morrischem.com>
2026-08-02 15:01:11 +0300

    Add theme setup and style enqueue functions

--- morrischem-industrial-clean/functions.php
+++ morrischem-industrial-clean/functions.php
@@ -1,1 +1,16 @@
+<?php
 
+function morrischem_enqueue_styles() {
+    wp_enqueue_style('morrischem-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'morrischem_enqueue_styles');
+
+function morrischem_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'morrischem'),
+    ));
+}
+add_action('after_setup_theme', 'morrischem_theme_setup');
